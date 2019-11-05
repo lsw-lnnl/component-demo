511b83ffd6d14232524327b80a505f9bf66d5f6c
lnnl <445257296@qq.com>
2019-11-05 19:06:14 +0800

    初始化

---
+++ src/components/searchInput/searchInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="search-input">
+        我是一个搜索框
+    </div>
+</template>
+<script>
+export default {
+    name: 'searchInput',
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+<style lang="less" scoped="">
+.search-input {}
+</style>
